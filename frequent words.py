cb5cc6187ecd81c034568b73762a31e808ca1bea
Aswin Pratapsingh <95207528+CodexEZ@users.noreply.github.com>
2022-08-07 11:49:26 +0530

    Update frequent words.py

--- frequent words.py
+++ frequent words.py
@@ -6,25 +6,26 @@ from cleantext import clean
 import operator
 
 reddit = praw.Reddit(
-    client_id="9uaf1tMjfGNKQniG5shFZA",
-    client_secret="Ka6bG6j9SB9JvUDW3cGLlNpjZ4aFCg",
-    user_agent="<console:CHAD:1.0>",
-    username="gigomegabot",
-    password="Ash@41938"
+    client_id="",#Enter client ID
+    client_secret="",#Enter your secret number
+    user_agent="",#console name
+    username="",#Enter your account username
+    password=""#Enter your password here
 )
 score=[]
 count=[]
-articles="a an the about above across after against that not just along among around at before behind between beyond but by concerning despite down during except following for from in including into like near of off on onto out over past plus since throughout to towards under until up upon up to with within without 🤓🤓🤓 🐒 is this [removed] me you and but because was so are what if else my how get"
-para=""
+#All the words that need to be excluded from the list since they dont have much value and are simple prepositions and articles
+articles="a an the about above across after against that not just they along among around at before behind between beyond but by concerning despite down during except following for from in including into like near of off on onto out over past plus since throughout to towards under until up upon up to with within without 🤓🤓🤓 🐒 is this [removed] me you and but because was so are what if else my how get"
+para=""#stores all the words in one paragraph
 c=1
-subbreddit=reddit.subreddit("memes")
-for post in subbreddit.hot(limit=10):
+subbreddit=reddit.subreddit("memes")#Opens subreddit r/memes
+for post in subbreddit.hot(limit=10):#Goes through top 10 hot posts
     for comment in post.comments:
 
         if isinstance(comment, MoreComments):
             continue
         lowercomment=comment.body.lower()
-        clean(lowercomment,no_emoji=False)
+        clean(lowercomment,no_emoji=True)
         clean(lowercomment,no_urls=True)
         clean(lowercomment,no_urls=True)
         lowercomment=lowercomment.split()
@@ -34,12 +35,12 @@ for post in subbreddit.hot(limit=10):
 
 unique=[]
 para = para.split()
-for i in para:
-    if i not in unique:
+for i in para:              
+    if i not in unique:         #Stores all the unique words in a list
         unique.append(i);
-count={}
+count={}#Stores all the unique words along with their frequencies
 for i in unique:
     count[i]=para.count(i)
-count = sorted(count.items(),key=operator.itemgetter(1), reverse=True)
-for i in range (5):
+count = sorted(count.items(),key=operator.itemgetter(1), reverse=True) #Sorts dictionary according to the values
+for i in range (5):#Here i have set it to show top 5 words in reddit comments
     print(count[i])
